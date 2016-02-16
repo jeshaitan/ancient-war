be1fdc05fec4e42eaa4625a9d46f79e6f6db2579
Jesse Smith <jesse.etan.smith@gmail.com>
2016-02-16 10:20:40 -0500

    added text input

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-var express = require('express');
+var express = require('express')
+  , bodyParser = require('body-parser');
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-  res.render('index', {actions: actions});
-});
-
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log('Ancient War listening on port 3000');
 });
 
-var actions = [{author: "Server", description: "Welcome to Ancient War."}
-              ,{author: "Server", description: "What is your name?"}]
+app.get('/', function(req, res) {
+  res.render('index', {history: history});
+});
+
+app.post('/do', function(req, res) {
+  if(history[history.length - 1].type == 'promptName') {
+    history.push({type: 'name', author: req.body.input, description: req.body.input});
+  }
+  res.render('index', {history: history});
+});
+
+var history = [{type: 'flavor', author: 'Server', description: 'Welcome to Ancient War.'}
+            , {type: 'promptName', author: 'Server', description: 'What is your name?'}]
