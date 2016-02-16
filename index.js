1c4f707e03f9c49255581e01430732ddc667f3d0
Jesse Smith <jesse.etan.smith@gmail.com>
2016-02-16 09:11:32 -0500

    created history and container

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {actions: actions});
 });
 
 app.listen(3000, function () {
   console.log('Ancient War listening on port 3000');
 });
+
+var actions = [{author: "Server", description: "Welcome to Ancient War."}
+              ,{author: "Server", description: "What is your name?"}]
