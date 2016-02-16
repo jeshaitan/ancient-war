0a9cb78b39fb219f8315db5b2904e99fc8477b3f
Jesse Smith <jesse.etan.smith@gmail.com>
2016-02-16 10:51:12 -0500

    word-wrap and new login

--- index.js
+++ index.js
@@ -19,11 +19,31 @@ app.get('/', function(req, res) {
 });
 
 app.post('/do', function(req, res) {
-  if(history[history.length - 1].type == 'promptName') {
-    history.push({type: 'name', author: req.body.input, description: req.body.input});
+  //login prompt
+  if(history[history.length - 1].type == 'login') {
+    if(isYes(req.body.input)) {
+      history.push({type: 'new', author: 'Unknown', description: req.body.input}
+                 , {type: 'namePrompt', author: 'Server', description: 'Greetings, what is your name?'});
+    }
+    else if(isNo(req.body.input)) {
+      history.push({type: 'old', author: 'Unknown', description: req.body.input}
+                 , {type: 'namePrompt', author: 'Server', description: 'Welcome back. Who is this again?'});
+    }
+    else {
+      history.push({type: 'other', author: 'Server', description: 'Sorry, I don\'t understand.'})
+    }
   }
+  //refresh history
   res.render('index', {history: history});
 });
 
 var history = [{type: 'flavor', author: 'Server', description: 'Welcome to Ancient War.'}
-            , {type: 'promptName', author: 'Server', description: 'What is your name?'}]
+            , {type: 'login', author: 'Server', description: 'Are you a new warrior?'}]
+
+function isYes(str) {
+  return (str == 'y') || (str == 'yes') || (str == 'Y') || (str == 'Yes')
+}
+
+function isNo(str) {
+  return (str == 'n') || (str == 'no') || (str == 'N') || (str == 'No')
+}
